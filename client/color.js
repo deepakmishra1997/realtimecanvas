0251e89e666fff3dcbf1682934e3f4cccc5b581c
deepakmishra <deepakmishra1997@gmail.com>
2020-03-21 11:00:57 +0530

    added soem changes

--- client/color.js
+++
@@ -1,9 +0,0 @@
-
-function changecolor(color)
-{
-    ctx.strokeStyle=color;
-    socket.emit("changescolor",color);
-}
-socket.on("change-color", function (color){
-    ctx.strokeStyle = color;
-})
--- client/uploaddownload.js
+++
@@ -1,7 +0,0 @@
-function download() {
-    var link = document.createElement('a');
-    link.download = 'filename.png';
-    link.href = document.querySelector('.board').toDataURL()
-    link.click();
-    socket.emit("d");
-}
